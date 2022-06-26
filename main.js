0837c4c86b8f9669c0bb578b65bb457461dad250
sabidhasannabil <sbaidhasan97@gmail.com>
2022-06-26 16:16:22 +0600

    add js file

---
+++ main.js
@@ -0,0 +1,19 @@
+// let, const
+
+// let age = 26;
+// age = 27;
+
+// console.log(age);
+
+// String, Numbers, Boolean, null, undefined
+
+const name = 'Nabil';
+const age = 26;
+const rating = 4.5;
+const isCool = true; 
+
+//Arrays
+
+const numbers = new Array(1, 2, 3, 4);
+
+console.log(numbers);
